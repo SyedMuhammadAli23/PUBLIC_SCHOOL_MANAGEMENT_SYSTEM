0b7a03c9949973178924bc158329f67865ae82f3
Muhammad Ali <muhammadali16032003@gmail.com>
2026-07-03 23:07:05 +0500

    devops dashboard env file changed 4

--- frontends/devops-dashboard/vite.config.js
+++ frontends/devops-dashboard/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_GATEWAY_URL': JSON.stringify('https://d1edzu3nj8f784.cloudfront.net')
+  },
   server: {
     port: 3002,
     host: true
