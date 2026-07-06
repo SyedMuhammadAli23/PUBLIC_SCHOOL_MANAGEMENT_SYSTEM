06b956b7471b7dfcbd93f7149ca7b8a6e71ce58b
Muhammad Ali <muhammadali16032003@gmail.com>
2026-07-06 16:42:16 +0500

    vite.config.js file updated 2

--- frontends/admin-portal/vite.config.js
+++ frontends/admin-portal/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin/',      // <-- add this
   plugins: [react()],
   server: {
-    host: true,
-    port: 3000
+    port: 3000,
+    host: true
   }
 })
--- frontends/user-portal/vite.config.js
+++ frontends/user-portal/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/user/',      // <-- add this
   plugins: [react()],
   server: {
     port: 3001,
