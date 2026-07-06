15a6cab1a782f5e63c57c74a452f0dc9beafed51
Muhammad Ali <muhammadali16032003@gmail.com>
2026-07-06 16:38:58 +0500

    vite.config.js file updated

--- frontends/devops-dashboard/vite.config.js
+++ frontends/devops-dashboard/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dashboard/',      // <-- add this
   plugins: [react()],
   define: {
     'import.meta.env.VITE_API_GATEWAY_URL': JSON.stringify('https://d1edzu3nj8f784.cloudfront.net')
